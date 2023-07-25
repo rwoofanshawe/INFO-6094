a476c9cb92f57b02202e8ca7bd3e1d4e9ad93d18
roben <roben@DESKTOP-TV6NF5J.mshome.net>
2023-07-24 22:59:22 -0400

    baseline project

---
+++ payroll_project/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author roben
+ *
+ */
+module payroll_project {
+}
---
+++ payroll_project/src/payroll_project/Payroll.java
@@ -0,0 +1,10 @@
+package payroll_project;
+
+public class Payroll {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
